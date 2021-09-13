ba68adf925548953150e8ee9b9ae488e251a1eab
lukerand <luke@randland.com>
2021-09-13 17:44:19 -0600

    Initial commit

---
+++ src/main/java/com/developer/manhunt/Manhunt.java
@@ -0,0 +1,19 @@
+package com.developer.manhunt;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Manhunt extends JavaPlugin {
+    Player target;
+    @Override
+    public void onEnable() {
+        getCommand()
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
